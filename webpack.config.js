56905fc8b561bce0d8a334e2154bff3675a6a58c
siemah <m_hamiche@inptic.edu.dz>
2020-08-01 05:45:17 +0100

    fix:proxying to the right port

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ const browserConfig = {
       // browse to http://localhost:3000/ during development,
       // ./public directory is being served
       host: 'localhost',
-      port: 3000,
+      port: 3001,
       proxy: 'http://localhost:3000/'
     }),
   ]
